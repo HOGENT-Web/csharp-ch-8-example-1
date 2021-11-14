31465901d90c3334ffbb9c08dd80c06e5e1f7d7b
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-11-14 20:49:41 +0100

    Delete Confirmation

--- src/Client/Products/Detail.razor.cs
+++ src/Client/Products/Detail.razor.cs
@@ -8,6 +8,7 @@ namespace Client.Products
     public partial class Detail
     {
         private ProductDto.Detail product;
+        private bool isRequestingDelete;
         [Parameter] public int Id { get; set; }
         [Inject] public IProductService ProductService { get; set; }
         [Inject] public NavigationManager NavigationManager { get; set; }
@@ -16,6 +17,16 @@ namespace Client.Products
             ProductRequest.GetDetail request = new() { ProductId = Id };
             var response = await ProductService.GetDetailAsync(request);
             product = response.Product;
+        }
+
+        private void RequestDelete()
+        {
+            isRequestingDelete = true;
+        }
+
+        private void CancelDeleteRequest()
+        {
+            isRequestingDelete = false;
         }
 
         private async Task DeleteProductAsync()
